8d1ed7b91f5105230866e64b89d57d5fb3878873
Алексей Верещагин <aleksejveresagin@MacBook-Pro-Aleksej.local>
2022-12-21 00:59:37 +0500

    add Task 15 Weekend or working day

---
+++ task_15/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.WriteLine("Enter the day odf the week!");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 8 || number < 1)
+{
+    Console.WriteLine("You entered an invalid value!");
+    return;
+}
+if (number > 5)
+{
+    Console.WriteLine("Weekend");
+}
+else
+{
+    Console.WriteLine("Working day");
+}
