e473c909e34bbead40f8c7015d729db05e91bfed
Алексей Верещагин <aleksejveresagin@MacBook-Pro-Aleksej.local>
2022-12-20 14:31:16 +0500

    add task 10 Enter a three digit number

--- task_10/Program.cs
+++ task_10/Program.cs
@@ -1,9 +1,11 @@
 ﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 Console.Clear();
-Console.WriteLine("Enter any three-digit number (from 100 to 999): ");
- int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
- string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("Enter a three-digit number (from 100 to 999): ");
+int ThreeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(ThreeDigitNumber);
 Console.WriteLine("The second digit this number: "+stringNumber[1]);
+
+
 
 
 
