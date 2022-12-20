0a6a74fef05ebd304aef974bf7a7f1841e0caf89
Алексей Верещагин <aleksejveresagin@MacBook-Pro-Aleksej.local>
2022-12-21 00:41:45 +0500

    add Task 13 Third digit

---
+++ task_13/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Clear();
+Console.WriteLine("Enter any number");
+string number = Console.ReadLine();
+if(number.Length < 3)
+{
+    Console.WriteLine("No third digit!");
+    return;
+}
+Console.WriteLine(number[2]);
